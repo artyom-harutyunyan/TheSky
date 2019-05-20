e32ca358d3eaef716db69e2773f8f9cb225d503c
artyom-harutyunyan <48210362+artyom-harutyunyan@users.noreply.github.com>
2019-05-20 19:10:31 +0500

    1

---
+++ main.js
@@ -0,0 +1,28 @@
+// var arr = [];
+// var rand = Math.round(Math.random() * 1000);
+
+// var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+// console.log(scrolled);
+// var elem = document.getElementsByClassName('element')[0];
+// var box = elem.getBoundingClientRect(); 
+// console.log(box);
+var arr = [];
+var container = document.getElementsByTagName('element');
+var ract = element.getBoundingClientRect();
+// var rand_x = Math.round(Math.random() * 1000);
+// var rand_y = Math.round(Math.random() * 1000);
+console.log(ract.top, ract.left, ract.right, ract.bottom);
+
+
+function generate() {
+
+
+}
+
+
+getBoundingClientRect()
+// var Gstar = document.createElement('div') * 10;
+// Gstar.className = 'star';
+// Gstar.style.left = rand_x;
+// Gstar.style.top = rand_y;
+// container.appendChild(Gstar);
